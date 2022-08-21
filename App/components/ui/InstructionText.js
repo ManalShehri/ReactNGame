49de928d2f706993ac5f3bb38ec9838d8ad64163
Manal Alshehri <manal.shehri.20@gmail.com>
2022-08-21 11:17:52 +0300

    part 68

--- App/components/ui/InstructionText.js
+++ App/components/ui/InstructionText.js
@@ -1,8 +1,12 @@
 import { StyleSheet, Text } from "react-native";
 import Colors from "../../constant/colors";
 
-function InstructionText({children}) {
-    return <Text style={styles.instructionText}>{children }</Text>;
+function InstructionText({children, style }) {
+    // here we pass the style from the GameScreen file (style) 
+    // we can put the styles in an arrays like bellow 
+    // the first index in the array will excute first which means the default will excute then the one from GameScreen
+    // the instructionText pass from here to gameScreen 
+    return <Text style={[styles.instructionText, style ]}>{children }</Text>;
 };
 
 export default InstructionText;
